f17f94f280b68d6f73ccbbe4e773b31bd3cfad87
Masashi Arino <masashi.arino@gmail.com>
2015-09-16 10:23:47 +0900

    作りかけファイルのコミット

---
+++ src/main/scala/Plugin.scala
@@ -0,0 +1,37 @@
+import javax.servlet.ServletContext
+
+import gitbucket.core.plugin.PluginRegistry
+import gitbucket.core.service.SystemSettingsService.SystemSettings
+import gitbucket.core.util.Version
+import local.indare.gitbucket.plugin.hook.controller.TestController
+
+class Plugin extends gitbucket.core.plugin.Plugin {
+  override val pluginId: String = "hook plugin"
+
+  override val pluginName: String = "Hook Plugin"
+
+  override val description: String = "Setting for remote repository hook script plugin"
+
+  override val versions: List[Version] = List(
+    Version(0, 1)
+  )
+
+  override def javaScripts(registry: PluginRegistry, context: ServletContext, settings: SystemSettings): Seq[(String, String)] = {
+    // Add Menu link to side menu
+    val path = settings.baseUrl.getOrElse(context.getContextPath)
+
+    Seq(
+    ".*/settings/options" -> s"""
+                                |$$('.nav.nav-tabs.nav-stacked.side-menu>li:last').after(
+                                |  $$('<li><a href="${path}/admin/hookscript">Hook Scripts</a></li>')
+                                |);
+      """.stripMargin
+    )
+  }
+
+
+  override val controllers = Seq(
+    "/admin/hookscript" -> new TestController()
+  )
+
+}
---
+++ src/main/scala/local/indare/gitbucket/plugin/hook/controller/TestController.scala
@@ -0,0 +1,30 @@
+package local.indare.gitbucket.plugin.hook.controller
+
+import local.arino.test.html
+import gitbucket.core.controller.ControllerBase
+import gitbucket.core.service.{RepositoryService, AccountService}
+import gitbucket.core.util.AdminAuthenticator
+import jp.sf.amateras.scalatra.forms._
+import org.slf4j.LoggerFactory
+
+
+class TestController extends TestControllerBase
+with AdminAuthenticator with RepositoryService
+
+trait TestControllerBase extends ControllerBase with AccountService with RepositoryService{
+  self: AdminAuthenticator =>
+
+  private val logger = LoggerFactory.getLogger(classOf[TestController])
+
+  case class ScriptForm(hookname: String, script: String)
+
+  private val scriptForm = mapping(
+    "hookname" -> trim(label("Hook Name", text(required))),
+    "script" -> trim(label("Hook Script", text(required)))
+  )(ScriptForm.apply)
+
+  get("/admin/hookscript"){
+    html.test(flash.get("info"))
+  }
+
+}
